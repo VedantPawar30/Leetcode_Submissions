6308d0aadc7541d113a1a27c793692007560ffdd
Vedant Pawar <pawarvedant968@gmail.com>
2026-07-23 19:06:17 +0530

    Add 2323_minimum_bit_flips_to_convert_number.cpp - LeetCode Problem 2323

---
+++ 2323_minimum_bit_flips_to_convert_number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int cnt=0;
+        int x = start^goal;
+        while(x){
+            cnt += x&1;
+            x=x>>1;
+        }
+        
+        return cnt;
+    }
+};
