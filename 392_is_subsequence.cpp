858e5286ab3de19a9f5cc665b8b5ebc05b780e08
Vedant Pawar <pawarvedant968@gmail.com>
2026-08-08 11:37:33 +0530

    Add 392_is_subsequence.cpp - LeetCode Problem 392

---
+++ 392_is_subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int idx=0;
+
+        for(char c:t){
+            if(c==s[idx]) idx++;
+            if(idx==s.size()) break;
+        }
+
+        if(idx==s.size()) return true;
+        return false;
+    }
+};
