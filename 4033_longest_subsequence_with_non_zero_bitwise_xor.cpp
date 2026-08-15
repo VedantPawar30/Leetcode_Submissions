c7c1a06f0614454804c4a79311b76262305a1573
Vedant Pawar <pawarvedant968@gmail.com>
2026-08-15 13:37:03 +0530

    Add 4033_longest_subsequence_with_non_zero_bitwise_xor.cpp - LeetCode Problem 4033

---
+++ 4033_longest_subsequence_with_non_zero_bitwise_xor.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int x=0;
+
+        for(int num : nums){
+            x=x^num;
+        }
+
+        if(x!=0) return nums.size();
+
+        else{
+            for(int num : nums){
+                if(num>0){
+                    return nums.size()-1;
+                }
+            }
+        }
+
+        return 0;
+
+
+    }
+};
